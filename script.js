3114ac8e7f1648dcd475c4a6f0100c186c994ab8
LAWRANCE MAVIMBELA <166739248+Lawrance247@users.noreply.github.com>
2024-11-11 17:39:10 +0200

    Add files via upload
    This is my profile. skills and project i've done.

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function toggleSkills(){
+    var skillsContent= document.getElementById("skillsContent");
+    if (skillsContent.style.display ==="none"){
+        skillsContent.style.display = "block";
+    }
+    else{
+        skillsContent.style.display = "none";
+    }
+}
+function toggleProjects(){
+    var projectsContent= document.getElementById("projectsContent");
+    if (projectsContent.style.display ==="none"){
+        projectsContent.style.display = "block";
+    }
+    else{
+        projectsContent.style.display = "none";
+    }
+}
